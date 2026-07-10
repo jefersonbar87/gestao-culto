aefe952aef9107b449584c95d7169fceaa6ae4e1
jefersonbar87 <jefersonbar87@gmail.com>
2026-07-10 20:31:31 -0300

    feat: atualização de interface e ícone do Trombetas & Festas, e outros ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -22,16 +22,19 @@ export default defineConfig(({ mode }) => {
         rollupOptions: {
           input: {
             main: path.resolve(__dirname, 'index.html'),
-            hymns: path.resolve(__dirname, 'hymns.html'),
-            gifts: path.resolve(__dirname, 'gifts.html'),
-            'other-apps': path.resolve(__dirname, 'other-apps.html'),
-            'service-data': path.resolve(__dirname, 'service-data.html'),
-            trombetas: path.resolve(__dirname, 'trombetas.html'),
-            'guest-list': path.resolve(__dirname, 'guest-list.html'),
-            prayer: path.resolve(__dirname, 'prayer.html'),
-            chat: path.resolve(__dirname, 'chat.html'),
+            home: path.resolve(__dirname, 'home.html'), // Adicionado com sucesso!
             login: path.resolve(__dirname, 'login.html'),
-            settings: path.resolve(__dirname, 'settings.html')
+            hymns: path.resolve(__dirname, 'hymns.html'),
+            prayer: path.resolve(__dirname, 'prayer.html'),
+            'guest-list': path.resolve(__dirname, 'guest-list.html'),
+            'service-data': path.resolve(__dirname, 'service-data.html'),
+            'service-attendance': path.resolve(__dirname, 'service-attendance.html'), // Adicionado com sucesso!
+            settings: path.resolve(__dirname, 'settings.html'),
+            'other-apps': path.resolve(__dirname, 'other-apps.html'),
+            'trombetas-invitation': path.resolve(__dirname, 'trombetas-invitation.html'), // Adicionado com sucesso!
+            gifts: path.resolve(__dirname, 'gifts.html'),
+            trombetas: path.resolve(__dirname, 'trombetas.html'),
+            chat: path.resolve(__dirname, 'chat.html')
           }
         }
       }
